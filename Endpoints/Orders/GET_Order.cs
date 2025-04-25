43b65a59e750dcb56922d44d1170330baca91fe2
Drew Crandall <drew@spectrumsolution.com>
2025-04-25 14:28:36 -0600

    added a GET Order method. trying to see if this is the best way to handle this

--- Endpoints/Orders/GET_Order.cs
+++ Endpoints/Orders/GET_Order.cs
@@ -1,6 +1,10 @@
-﻿using ExtensivSharp.Models.Orders;
+﻿using ExtensivSharp.Entities;
+using ExtensivSharp.Models.Orders;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
@@ -8,12 +12,53 @@ namespace ExtensivSharp.Endpoints.Orders
 {
     public class GET_Order
     {
+        public string? AuthorizationToken { get; set; }
         public int OrderId { get; set; }
         public SpecifyDetailType Detail { get; set; } = SpecifyDetailType.None;
         public SpecifyItemDetailType ItemDetail { get; set; } = SpecifyItemDetailType.None;
-        public string ToUrl()
+        public async Task<ExtensivApiResult<Order>> GetAsync()
         {
-            return $"https://secure-wms.com/orders/{OrderId}?detail={Detail}&itemdetail={ItemDetail}";
+            using (HttpClient client = new HttpClient())
+            {
+                var result = new ExtensivApiResult<Order>()
+                {
+                    Success = false
+                };
+                var url = $"https://secure-wms.com/orders/{OrderId}?detail={Detail}&itemdetail={ItemDetail}";
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/hal+json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", AuthorizationToken);
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                result.StatusCode = response.StatusCode;
+
+                if (response.IsSuccessStatusCode) 
+                {
+                    result.Success = true;
+                    result.Data = JsonConvert.DeserializeObject<Order>(responseContent);
+                }
+                else
+                {
+                    switch (response.StatusCode)
+                    {
+                        case HttpStatusCode.Unauthorized:
+                            result.Message = "Authentication failed. Please check your credentials.";
+                            break;
+                        case HttpStatusCode.Forbidden:
+                            result.Message = "You don't have permission to access this resource.";
+                            break;
+                        case HttpStatusCode.NotFound:
+                            result.Message = "The requested resource was not found.";
+                            break;
+                        default:
+                            result.Message = $"API error: {response.StatusCode} - {responseContent}";
+                            break;
+                    }
+                }
+                return result;
+            }
         }
     }
 }
---
+++ Entities/ExtensivApiResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ExtensivSharp.Entities
+{
+    public class ExtensivApiResult<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public T? Data { get; set; }
+
+        public bool IsSuccess() => Success;
+        public bool IsUnauthorized() => StatusCode == HttpStatusCode.Unauthorized;
+        public bool IsNotFound() => StatusCode == HttpStatusCode.NotFound;
+    }
+}
