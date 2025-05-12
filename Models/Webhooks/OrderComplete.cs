5fb2932e53dd46c786122cb8c51e7111c886b202
Drew Crandall <drew@spectrumsolution.com>
2025-05-12 17:17:21 -0600

    adding a new webhook data structure

---
+++ Models/Webhooks/OrderComplete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensivSharp.Models.Webhooks
+{
+    public class OrderComplete
+    {
+        public int TplId { get; set; }
+        public int WmsEventId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string EventType { get; set; }
+        public Resource Resource { get; set; }
+        public string Links { get; set; }
+        public string Data { get; set; }
+        public string Tags { get; set; }
+    }
+}
---
+++ Models/Webhooks/Resource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensivSharp.Models.Webhooks
+{
+    public class Resource
+    {
+        public string Rel { get; set; }
+        public string Href { get; set; }
+        public Order.Order Body { get; set; }
+    }
+}
